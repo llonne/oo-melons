cb3866b4cdd0edae132bf9129a8c6b85b6250846
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-13 12:44:38 -0800

    Completed Part 4.

--- melons.py
+++ melons.py
@@ -1,8 +1,10 @@
 """This file should have our order classes in it."""
+from random import randint
 
 
 class AbstractMelonOrder(object):
     """A melon order template"""
+
 
     def __init__(self, species, qty, country_code, order_type, tax):
         """Initialize melon order"""
@@ -13,10 +15,14 @@ class AbstractMelonOrder(object):
         self.order_type = order_type
         self.tax = tax
 
+    def get_base_price(self):
+        base_price = randint(5, 9)
+        return base_price
+
     def get_total(self):
         """Calculate price."""
+        base_price = self.get_base_price()
 
-        base_price = 5
         total = (1 + self.tax) * self.qty * base_price
         if self.species is 'Christmas':
             total = (1 + self.tax) * self.qty * (base_price * 1.5)
@@ -55,10 +61,11 @@ class GovernmentMelonOrder(AbstractMelonOrder):
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
+    
 
     def __init__(self, species, qty):
         """Initialize melon order attributes"""
-
+        
         super(DomesticMelonOrder, self).__init__(species, qty, 'USA', 'domestic', 0.08)
 
 
