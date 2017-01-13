048261a39d366a581f47cc3898b514ce62ebc737
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-12 17:46:11 -0800

    Completed Part 2.

--- melons.py
+++ melons.py
@@ -4,19 +4,33 @@
 class AbstractMelonOrder(object):
     """A melon order template"""
 
-    def __init__(self, species, qty, country_code):
+    def __init__(self, species, qty, country_code, order_type, tax):
         """Initialize melon order"""
         self.species = species
         self.qty = qty
         self.shipped = False
         self.country_code = country_code
+        self.order_type = order_type
+        self.tax = tax
 
     def get_total(self):
         """Calculate price."""
 
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
-        return total
+        if self.species is 'Christmas':
+            total = (1 + self.tax) * self.qty * (base_price * 1.5)
+
+            if self.order_type is 'international' and self.qty < 10:
+                return total + 3
+
+            return total
+
+        elif self.order_type is 'international' and self.qty < 10:
+            return total + 3
+
+        else:
+            return total
 
     def mark_shipped(self):
         """Set shipped to true."""
@@ -30,9 +44,7 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty):
         """Initialize melon order attributes"""
 
-        super(DomesticMelonOrder, self).__init__(species, qty, 'USA')
-        self.order_type = "domestic"
-        self.tax = 0.08
+        super(DomesticMelonOrder, self).__init__(species, qty, 'USA', 'domestic', 0.08)
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -41,10 +53,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
 
-        super(InternationalMelonOrder, self).__init__(species, qty, country_code)
-        self.order_type = "international"
-        self.tax = 0.17
-
+        super(InternationalMelonOrder, self).__init__(species, qty, country_code, 'international', 0.17)
 
     def get_country_code(self):
         """Return the country code."""
